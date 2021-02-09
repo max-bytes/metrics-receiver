f4cfb9078a069361c7cd35370920ccfe765b34f2
Muhamet Kaçuri <MuhametKauri@LAPTOP-CO0D0C5E>
2021-02-09 20:54:42 +0100

    clean up

--- main.go
+++ main.go
@@ -376,29 +376,3 @@ func MakeRange(min, max int) []string {
 	}
 	return a
 }
-
-func In_array(needle interface{}, hystack interface{}) bool {
-	switch key := needle.(type) {
-	case string:
-		for _, item := range hystack.([]string) {
-			if key == item {
-				return true
-			}
-		}
-	case int:
-		for _, item := range hystack.([]int) {
-			if key == item {
-				return true
-			}
-		}
-	case int64:
-		for _, item := range hystack.([]int64) {
-			if key == item {
-				return true
-			}
-		}
-	default:
-		return false
-	}
-	return false
-}
